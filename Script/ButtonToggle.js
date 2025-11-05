7e317e71fc779d0de66ca5df5530471427775ce7
iftakhersifat <hossensifat615@gmail.com>
2025-11-05 18:53:45 +0600

    added button toggle

---
+++ Script/ButtonToggle.js
@@ -0,0 +1,6 @@
+document.getElementById("backToTop").addEventListener("click", function () {
+    document.getElementById("home").scrollIntoView({ behavior: "smooth" })
+})
+document.getElementById("navBalanceContainer").addEventListener("click", function () {
+    document.getElementById("userBalance").scrollIntoView({ behavior: "smooth" })
+})
